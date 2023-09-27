d813363e63f8ec7bc988fb5203e910a882e7fd90
Yolo <136174825+YamanSD@users.noreply.github.com>
2023-09-27 14:05:13 +0300

    Bug fix

--- src/services/helpers/formatters.ts
+++ src/services/helpers/formatters.ts
@@ -1,7 +1,16 @@
 /**
- * @param value to be formatted
+ * @param value number in string form, parsed and formatted
  * @returns a string of the value rounded to the nearest hundredth.
+ *          If the given value is not a valid number, the string is returned as is.
  */
-export function formatValue(value: number): string {
-    return value.toFixed(2);
+export function formatValue(value: string): string {
+    /* if invalid this variable is NaN */
+    const actualValue: number = Number(value);
+
+    if (isNaN(actualValue)) {
+        return value;
+    }
+
+    /* value is valid */
+    return actualValue.toFixed(2);
 }
