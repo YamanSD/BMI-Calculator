6cabe9a7eef9c1cd7a1013bf568c86e3922d83d1
Yolo <136174825+YamanSD@users.noreply.github.com>
2023-09-27 17:30:16 +0300

    Improved styling

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,58 @@
-import React from "react";
-import "./HomeScreen.css";
+import React, {useState} from "react";
+import styles from "./HomeScreen.module.css";
 import {NumericInput} from "../../components";
+import {generateNumericInputErr, isPositive} from "../../services";
 
+/**
+ * Home screen for the BMI calculator site.
+ * @constructor
+ */
 const HomeScreen = () => {
+    /* state for the weight of the user, in kilograms */
+    const [weight, setWeight] = useState<string>("");
+
+    /* state for the height of the user, in meters */
+    const [height, setHeight] = useState<string>("");
+
     return (
-      <div>
-          <NumericInput />
-      </div>
+        <>
+            <div className={styles.app__bar}>
+                <h2 className={styles.bar__title}>
+                    BMI.net
+                </h2>
+            </div>
+            <div className={styles.surface}>
+                <div className={styles.input__container}>
+                     <NumericInput value={weight}
+                                  setValue={setWeight}
+                                  isValid={isPositive}
+                                  label={"Weight"}
+                                  unit={"Kg"}
+                                  generateErrorMsg={generateNumericInputErr}
+                                  className={styles.input__field}
+                    />
+
+                    <NumericInput value={height}
+                                  setValue={setHeight}
+                                  isValid={isPositive}
+                                  label={"Height"}
+                                  unit={"m"}
+                                  generateErrorMsg={generateNumericInputErr}
+                                  className={styles.input__field}
+                    />
+                </div>
+
+                <div className={styles.button__container}>
+                    <button>
+                        Calculate
+                    </button>
+
+                    <button>
+                        Clear
+                    </button>
+                </div>
+           </div>
+        </>
     );
 };
 
