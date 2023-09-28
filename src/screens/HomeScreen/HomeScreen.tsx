69d6ca124a989f01b60d3f8c3533af3536084e4a
Yolo <136174825+YamanSD@users.noreply.github.com>
2023-09-29 00:29:19 +0300

    Implemented progressBar

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,7 +1,15 @@
 import React, {useState} from "react";
 import styles from "./HomeScreen.module.css";
 import {AnimatedButton, NumericInput} from "../../components";
-import {generateNumericInputErr, isPositive} from "../../services";
+import {
+    BmiLevel,
+    calculateBmi,
+    calculateBmiLevel,
+    formatValue,
+    generateNumericInputErr,
+    isPositive
+} from "../../services";
+import "./ProgressBar.css";
 
 /**
  * Home screen for the BMI calculator site.
@@ -14,6 +22,14 @@ const HomeScreen = () => {
     /* state for the height of the user, in meters */
     const [height, setHeight] = useState<string>("");
 
+    /*
+     * state for the current value of BMI.
+     * Undefined indicates not calculated before.
+     * NaN indicates the give parameters are invalid.
+     * A non-positive value is automatically transformed to NaN.
+     */
+    const [bmi, setBmi] = useState<number | undefined>(undefined);
+
     /**
      * Clears the height and weight fields.
      */
@@ -22,8 +38,50 @@ const HomeScreen = () => {
         setHeight('');
     }
 
+    /**
+     * Calculates the new BMI based on the height and width.
+     */
+    const onClickCalculate = () => {
+        setBmi(calculateBmi(Number(weight), Number(height)));
+    }
+
+    /**
+     * @param bmi state variable representing user BMI
+     * @returns the suitable color for the BMI based on its range.
+     */
+    const getLevelColor = (bmi: number) => {
+        switch (calculateBmiLevel(bmi)) {
+            case BmiLevel.underweight:
+                return "#ff9900";
+            case BmiLevel.healthy:
+                return "#007570";
+            case BmiLevel.overweight:
+                return "#ff0000";
+            case BmiLevel.obese:
+                return "#800000";
+        }
+    }
+
+    /**
+     * @param bmi state variable representing user BMI.
+     * @returns the category of the calculated BMI, as a string.
+     */
+    const getLevelMessage = (bmi: number) => {
+        switch (calculateBmiLevel(bmi)) {
+            case BmiLevel.underweight:
+                return "Underweight";
+            case BmiLevel.healthy:
+                return "Healthy";
+            case BmiLevel.overweight:
+                return "Overweight";
+            case BmiLevel.obese:
+                return "Obese";
+        }
+    }
+
     return (
         <div className={styles.screen}>
+
             <div className={styles.app__bar}>
                 <h2 className={styles.bar__title}>
                     BMI.net
@@ -55,9 +113,56 @@ const HomeScreen = () => {
                                     onClick={onClickClear}
                     />
                     <AnimatedButton title={"Calculate"}
-
+                                    onClick={onClickCalculate}
                     />
                 </div>
+
+                {/* BMI information */}
+                {
+                    bmi !== undefined ? (
+                        <div className={styles.bmi__container}>
+                            {
+                                !isNaN(bmi) ? (
+                                    <>
+                                        <div
+                                        role="progressbar"
+                                        aria-valuenow={bmi}
+                                        aria-valuemin={0}
+                                        aria-valuemax={100}
+                                        style={{
+                                            '--value': 2 * bmi,
+                                            '--color': getLevelColor(bmi)
+                                        } as any}
+                                    ></div>
+                                    <text className={styles.bmi__value}
+                                          style={{
+                                              '--bmi-label-color': getLevelColor(bmi)
+                                          } as any}>
+                                        BMI = {
+                                            formatValue(bmi)
+                                        }
+                                    </text>
+                                    <br/>
+                                    <text className={styles.bmi__value}
+                                          style={{
+                                              '--bmi-label-color': getLevelColor(bmi)
+                                          } as any}>
+                                        ({getLevelMessage(bmi)})
+                                    </text>
+                                </>
+                            ) : (
+                                <text className={styles.bmi__value}
+                                      style={{
+                                          '--bmi-label-color': "#800000"
+                                      } as any}
+                                >
+                                    Invalid input
+                                </text>
+                            )
+                            }
+                        </div>
+                    ) : null
+                }
            </div>
         </div>
     );
