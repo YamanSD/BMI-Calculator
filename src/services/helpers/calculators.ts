d634f9ebfa90a8f05cd7d38941e0d516bbe359a3
Yolo <136174825+YamanSD@users.noreply.github.com>
2023-09-29 00:28:19 +0300

    Implemented BmiLevels and their calculator

--- src/services/helpers/calculators.ts
+++ src/services/helpers/calculators.ts
@@ -6,3 +6,31 @@
 export function calculateBmi(weight: number, height: number): number {
     return weight / (height * height);
 }
+
+/**
+ * Enum for the upper-bounds of BMI ranges.
+ *
+ * Ranges are based on this link https://www.cdc.gov/healthyweight/assessing/index.html
+ */
+export enum BmiLevel {
+    underweight = 18.5,
+    healthy = 25,
+    overweight = 30,
+    obese = Infinity
+}
+
+/**
+ * @param bmi calculated BMI of the user.
+ * @returns the BmiLevel corresponding to the given BMI.
+ */
+export function calculateBmiLevel(bmi: number): BmiLevel {
+    if (bmi < BmiLevel.underweight) { // underweight
+        return BmiLevel.underweight;
+    } else if (bmi < BmiLevel.healthy) { // healthy
+        return BmiLevel.healthy;
+    } else if (bmi < BmiLevel.overweight) { // overweight
+        return BmiLevel.overweight;
+    } else { // obese
+        return BmiLevel.obese;
+    }
+}
